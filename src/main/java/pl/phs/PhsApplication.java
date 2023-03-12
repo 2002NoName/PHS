fa73b6cffa9e361a2f7eefb6c9a0c279e66a9761
2002NoName <kozienskimichal@gmail.com>
2023-03-12 20:29:09 +0100

    First commit

---
+++ src/main/java/pl/phs/PhsApplication.java
@@ -0,0 +1,13 @@
+package pl.phs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/phs/PhsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.phs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
